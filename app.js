7a7f23dd4ecbafec551b83be2ba1f833fc4dbf27
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-07-23 03:44:06 +0100

    Added users model, signup and delete route

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const mongoose = require('mongoose');
 // Routes which should handle requests
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/users');
 
 mongoose.connect(
     'mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@cluster0.iuxzb.mongodb.net/<dbname>?retryWrites=true&w=majority', 
     {
-        useNewUrlParser: true, useUnifiedTopology: true
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
     }
 );
 
@@ -32,6 +35,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
